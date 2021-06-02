30e6c629f597b273ef6d129da5d1cbda31a38e99
Yana Hontarenko <y.hontarenko@spd-ukraine.com>
2021-06-02 16:38:44 +0300

    Create analyzer class

---
+++ analyzer.py
@@ -0,0 +1,29 @@
+import boto3
+import io
+
+
+class Analyzer:
+
+    def __init__(self, aws_access_key_id, aws_secret_access_key, region_name):
+        self.s3_resource = boto3.resource('s3',
+                                          aws_access_key_id=aws_access_key_id,
+                                          aws_secret_access_key=aws_secret_access_key,
+                                          region_name=region_name)
+        self.textract_client = boto3.client('textract',
+                                          aws_access_key_id=aws_access_key_id,
+                                          aws_secret_access_key=aws_secret_access_key,
+                                          region_name=region_name)
+
+    def _get_data_from_bucket(self, bucket, image_name):
+        s3_object = self.s3_resource.Object(bucket, image_name)
+        s3_response = s3_object.get()
+        stream = io.BytesIO(s3_response['Body'].read())
+        image_binary = stream.getvalue()
+        return image_binary
+
+    def analyze_image(self, bucket, image_name):
+        image_binary = self._get_data_from_bucket(bucket, image_name)
+        response = self.textract_client.analyze_document(Document={'Bytes': image_binary},
+                                                         FeatureTypes=["TABLES", "FORMS"])
+        blocks = response['Blocks']
+        return image_binary, blocks
